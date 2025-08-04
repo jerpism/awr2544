62a9d52e569613415497766f71ac7dfa7c840c2a
Matias Karjalainen <t1kama00@students.oamk.fi>
2025-08-04 12:31:22 +0300

    pcap parser parses whole frame + plots all chirps

--- tools/pcap_parser.py
+++ tools/pcap_parser.py
@@ -1,17 +1,47 @@
-#file to be parsed is named frame.pcapng
-
 import dpkt
 import numpy as np
 import matplotlib.pyplot as plt
-import copy
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fname", help="name of .pcap file to parse")
+args = parser.parse_args()
+
+filename=args.fname
+range_res = 0.39
 
 counter=0
-ipcounter=0
 udpcounter=0
-
-filename='frame.pcap'
-
+chirpcounter=0
 pktbuf=0
+
+rx1_comp = np.zeros(128, dtype=np.complex128)
+rx2_comp = np.zeros(128, dtype=np.complex128)
+rx3_comp = np.zeros(128, dtype=np.complex128)
+rx4_comp = np.zeros(128, dtype=np.complex128)
+
+#compute x axis range values
+ranges = np.arange(rx1_comp.shape[0]) * range_res
+
+plt.ion()
+fig, axs = plt.subplots(2,2)
+lines = []
+fig.suptitle("Each rx for chrip: ")
+for ax in axs.flat:
+    ax.set(xlabel='range', ylabel='signal component amp')
+    ax.label_outer()
+    line, = ax.plot(np.zeros(128))
+    lines.append(line)
+
+
+def getChripData(buf,rx1,rx2):
+    for i in range(len(rx1)):
+        idx = 4*i
+        rx1[i] = int.from_bytes(buf[idx:idx+2],"little",signed=True) + (1j*int.from_bytes(buf[idx+2:idx+4],"little",signed=True))
+        idx = 512 + 4*i
+        rx2[i] = int.from_bytes(buf[idx:idx+2],"little",signed=True) + (1j*int.from_bytes(buf[idx+2:idx+4],"little",signed=True))
+    return
+
 
 for ts, pkt in dpkt.pcap.Reader(open(filename,'rb')):
 
@@ -21,69 +51,33 @@ for ts, pkt in dpkt.pcap.Reader(open(filename,'rb')):
        continue
 
     ip=eth.data
-    ipcounter+=1
 
     if ip.p==dpkt.ip.IP_PROTO_UDP:
-        udpcounter+=1
-        if udpcounter==2:
+        #drop first packet
+        if (udpcounter>=1):
             pktbuf = ip.data.data
+            if (udpcounter%2 == 1):
+                getChripData(pktbuf,rx1_comp,rx2_comp)
+            if (udpcounter%2 == 0):
+                getChripData(pktbuf,rx3_comp,rx4_comp)
+                chirpcounter+=1
+                #when data from each rx is done, plot new data
+                string = "Each rx for chirp: " + str(chirpcounter)
+                fig.suptitle(string)
+                for i in range(4):
+                    cmplx_sets = [rx1_comp,rx2_comp,rx3_comp,rx4_comp]
+                    lines[i].set_ydata(np.abs(cmplx_sets[i]))
+                    lines[i].set_xdata(ranges)
+                for ax in axs.flat:
+                    ax.relim()
+                    ax.autoscale_view()
+                fig.canvas.draw()
+                fig.canvas.flush_events()
 
-#get one chirp data:
-print(type(pktbuf),type(pktbuf[0]),len(pktbuf))
-testvar = int.from_bytes(pktbuf[:1])
-print(pktbuf[:1],print(type(pktbuf[:1])),testvar)
-
-rx1_comp = np.zeros(128,dtype=np.complex128)
-real_arr = np.zeros(128)
-img_arr = np.zeros(128)
-
-for i in range(len(rx1_comp)):
-    idx = 4*i
-    rx1_comp[i] = int.from_bytes(pktbuf[idx:idx+2],"little",signed=True) + (1j*int.from_bytes(pktbuf[idx+2:idx+4],"little",signed=True))
-    real_arr[i] = int.from_bytes(pktbuf[idx:idx+2],"little",signed=True)
-    img_arr[i] = int.from_bytes(pktbuf[idx+2:idx+4],"little",signed=True)
-
-print(real_arr)
-print(img_arr)
-print(rx1_comp[0])
-print(type(rx1_comp),type(rx1_comp[0]))
-
-plt.figure()
-plt.plot(np.abs(rx1_comp))
-plt.title("Range profile")
-plt.xlabel("Range (m)")
-plt.ylabel("Corresponding frequency component intensity")
-plt.grid(True)
-plt.show()
+        udpcounter+=1
 
 '''
-rx1_re = pktbuf[:512:4] + pktbuf[1:512:4]
-rx1_im = pktbuf[2:512:4] + pktbuf[3:512:4]
-
-for i in range(len(rx1_comp)):
-    if i == 0:
-        arr_re = (rx1_re[i],rx1_re[i+1])
-        arr_im = (rx1_im[i],rx1_im[i+1])
-    else:
-        arr_re = (rx1_re[(i*2)-1],rx1_re[i*2])
-        arr_im = (rx1_im[(i*2)-1],rx1_im[i*2])
-        rx1_comp[i] = int.from_bytes(arr_re,signed=True) + (1j*int.from_bytes(arr_im,signed=True))
-
-
-
-
-print("Total number of packets in the pcap file: ", counter)
-print("Total number of ip packets: ", ipcounter)
-print("Total number of udp packets: ", udpcounter)
-
-
-with open((filename), 'rb') as f:
-    pcap = dpkt.pcap.Reader(f)
-    pktBuf = pcap.readpkts()
-    print(type(pktBuf))
-    print(len(pktBuf))
-
-    print(type(pktBuf[2][1]))
-    print(pktBuf[2][1])
-
+print("all pkts counter: ", counter," udpcounter: ", udpcounter)
+print("pktbuf, size:", len(pktbuf)," type:",type(pktbuf)," type of element:",type(pktbuf[0:1]))
+print("rx1_comp, size:", len(rx1_comp)," type:",type(rx1_comp)," type of element:",type(rx1_comp[0]))
 '''
