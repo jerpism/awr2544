80e587ac10ccf94bceb2537040e6d6b2e7a229f2
jerpism <jere.m@live.fi>
2025-08-19 14:28:07 +0300

    Add function to arrange the chirps for a receiver into a matrix (really just copies e.g. rx0 data from every chirp)

--- c-src/dataprocessing.c
+++ c-src/dataprocessing.c
@@ -13,3 +13,21 @@ void calc_abs_vals(int16_t *in, int16_t *out, uint32_t n){
 
     }
 }
+
+
+void arrange_matrix(uint32_t *in, size_t n, size_t m, uint32_t *out){
+    // Assume we're using 16 bit complex data
+    // so we can just move 4 bytes at once to copy an entire sample
+    // Number of elements per one rx in a chirp if we're using them as 4 byte values
+    size_t rx_elements = CHIRP_DATASIZE / NUM_RX_ANTENNAS /  sizeof(uint32_t);
+    // How many we have in a chirp
+    size_t chirp_elements = CHIRP_DATASIZE / sizeof(uint32_t);
+
+    // Arrange chirps for one receiver into a [n][m] matrix
+    for(size_t i = 0; i < CHIRPS_PER_FRAME; ++i){
+        for(size_t j = 0; j < rx_elements; ++j){
+            out[i * rx_elements + j] = in[i * chirp_elements + j];
+        }
+    }
+
+}
