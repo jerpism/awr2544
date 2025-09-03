040bfa4eb0bc2e635569849d3c15c357a98f8ee8
jerpism <jere.m@live.fi>
2025-09-03 16:30:05 +0300

    Remove conflicting stuff from sandbox.

--- c-src/sandbox.c
+++ c-src/sandbox.c
@@ -22,46 +22,6 @@
 #include "ti_drivers_open_close.h"
 #include "ti_board_open_close.h"
 
-void reflect_pad(int16_t *input, size_t n, int radius, int16_t *padded){
-    DebugP_assert(radius < n);
-    // Pad the beginning of the array
-    // e.g. in={1,2,3,4} radius=2 -> padded={3,2}
-    for(int i = 0; i < radius; ++i){
-        padded[i] = input[radius - i];
-    }
-
-    // Fill in the already existing values
-    // same as before -> padded={3,2,1,2,3,4}
-    for(int i = 0; i < n; ++i){
-        padded[radius + i] = input[i];
-    }
-
-    // And then pad the end of the array
-    // same as before -> padded={3,2,1,2,3,4,3,2}
-    for(int i = 0; i < radius; ++i){
-        padded[radius + n + i] = input[n - 2 - i];
-    }
-
-}
-
-void convolve_1d(int16_t *a, int n, int16_t *b, int m, int16_t *out){
-    int radius = m / 2;
-    int padded_len = 2 * radius + n;
-    int16_t *padded;
-    padded = malloc(padded_len * sizeof(int16_t));
-    reflect_pad(a, n, radius, padded);
-    
-    for(int i = 0; i < n; ++i){
-        // Maybe just calloc or memset? probably faster
-        out[i] = 0;
-        for(int j = 0; j < m; ++j){
-            out[i] += padded[i+j] * b[j];
-        }
-    }
-
-    free(padded);
-}
-
 void sandbox_main(void *args){
     int win_sz = 2;
     float ma[40];
